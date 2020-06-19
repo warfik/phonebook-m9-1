ea22606d6ef0a4a74220af35079c20861fa57a27
aliviaVi <49653661+aliviaVi@users.noreply.github.com>
2020-06-19 12:58:52 +0200

    email service (#12)
    * email service

---
+++ phonebook-api/src/main/java/com/telran/phonebookapi/service/EmailSenderService.java
@@ -0,0 +1,32 @@
+package com.telran.phonebookapi.service;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+
+public class EmailSenderService {
+
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String mailFrom;
+
+    public EmailSenderService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+    @Async
+    public void sendMail(String mailTo, String mailFrom, String subject, String message){
+        SimpleMailMessage mailMessage=new SimpleMailMessage();
+
+        mailMessage.setFrom(mailFrom);
+        mailMessage.setTo(mailTo);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+
+        javaMailSender.send(mailMessage);
+    }
+
+
+}
---
+++ phonebook-api/src/test/java/com/telran/phonebookapi/service/EmailSenderServiceTest.java
@@ -0,0 +1,52 @@
+package com.telran.phonebookapi.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class EmailSenderServiceTest {
+    @InjectMocks
+    EmailSenderService emailSenderService;
+    @Mock
+    JavaMailSender javaMailSender;
+    @Captor
+    private ArgumentCaptor<SimpleMailMessage> messageArgumentCaptor;
+    @Value("${spring.mail.username}")
+    String mailFrom = "mailFrom";
+
+
+    @Test
+    public void send_mail2() {
+
+        String mailTo = "mailTo";
+        String text = "text";
+        String subject = "subject";
+
+
+        emailSenderService.sendMail(mailTo, mailFrom, subject, text);
+
+        verify(javaMailSender, times(1)).send(messageArgumentCaptor.capture());
+
+        SimpleMailMessage capturedMessage = messageArgumentCaptor.getValue();
+
+        // assertEquals(mailFrom, capturedMessage.getFrom());
+        assertEquals(mailFrom, capturedMessage.getFrom());
+        assertEquals(mailTo, Objects.requireNonNull(capturedMessage.getTo())[0]);
+        assertEquals(text, capturedMessage.getText());
+        assertEquals(subject, capturedMessage.getSubject());
+    }
+}
